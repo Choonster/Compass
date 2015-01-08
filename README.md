00d864e710927d0736f36a099586de5b928bd3f8
Choonster <Choonster.2010@gmail.com>
2015-01-09 01:12:00 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+== Usage ==
+Hold Ctrl and drag the compass to move it. The arrowhead points north (it's a compass).
+
+== Installation ==
+* Click the "Download ZIP" button on GitHub
+* Extract the downloaded ZIP
+* Rename the innermost folder (containing the AddOn files) to `Compass`
+* Copy that folder into your `WoW\Interface\AddOns` folder.
+* Restart WoW if it was open.
---
+++ core.lua
@@ -0,0 +1,32 @@
+local FRAME_SIZE = 140
+local TEXTURE_SIZE = FRAME_SIZE * 2 ^ 0.5 -- Make texture widget bigger to keep original texture size (suggested by Wowpedia page for Texture:SetRotation)
+
+local frame = CreateFrame("Frame", "CompassFrame", UIParent)
+frame:SetSize(140, 140)
+frame:SetPoint("CENTER")
+
+frame:EnableMouse(true)
+frame:SetMovable(true)
+frame:SetUserPlaced(true)
+frame:RegisterForDrag("LeftButton")
+
+frame:SetScript("OnDragStart", function(self)
+	print("DragStart")
+	if IsControlKeyDown() then
+		self:StartMoving()
+	end
+end)
+
+frame:SetScript("OnDragStop", function(self)
+	print("DragStop")
+	self:StopMovingOrSizing()
+end)
+
+local texture = frame:CreateTexture()
+texture:SetSize(TEXTURE_SIZE, TEXTURE_SIZE)
+texture:SetTexture("Interface\\AddOns\\Compass\\Compass")
+texture:SetPoint("CENTER")
+
+frame:SetScript("OnUpdate", function(self, elapsed)
+	texture:SetRotation(GetPlayerFacing())
+end)
